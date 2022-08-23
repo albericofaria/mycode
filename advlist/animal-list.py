34be2a66660e3a8fe01a4406cacd5fc9120c841d
albericofaria <al.faria@gmail.com>
2022-08-23 14:21:52 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+    # create a list called animal_list
+    animal_list = [Fox Fly Ant Bee Cod Cat Dog Yak Cow Hen Koi Hog Jay Kit]
+    
+    # display animal_list
+    print(animal_list)
